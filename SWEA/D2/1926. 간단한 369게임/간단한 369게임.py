6c2fca3629eb592ca5ff155d70e2dfb23a504df8
Shinbi Park <127182105+sinbii@users.noreply.github.com>
2025-11-22 11:02:18 +0900

    [D2] Title: 간단한 369게임, Time: 58 ms, Memory: 51,328 KB -BaekjoonHub

---
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+for i in range(1, N + 1):
+    s = str(i)
+    count = s.count('3') + s.count('6') + s.count('9')
+
+    if count > 0:
+        print('-' * count, end=' ')
+    else:
+        print(i, end=' ')
